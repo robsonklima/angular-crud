92af34f83d7874922b7ee7ae807aa656646f9d51
Escritorio <Escritorio@MACBOOK>
2021-05-07 00:06:27 -0300

    REF

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ const app = express();
 // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
 app.use(express.static(__dirname + "/dist/angular-crud"));
 
-app.get('*', function (req, res) {
-  const index = path.join(__dirname, 'build', 'index.html');
-  res.sendFile(index);
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/angular-crud/index.html"));
 });
 
 // Inicia a aplicação pela porta configurada
